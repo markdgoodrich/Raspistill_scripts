45506937f70c879f1ab2e1065714dfb24747f360
Mark Goodrich <mark.d.goodrich@gmail.com>
2020-07-10 09:30:48 -0400

    Adding Interval & normal Photo scripts

---
+++ Interval.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import os
+from picamera import PiCamera
+import time
+
+#Take a photo
+#name it
+#add it to/var/www/html/
+
+camera = PiCamera()
+camera.resolution = (2592,1944)
+path = "/var/www/html/Interval_" + datetime.now().strftime('%Y%m%d') + "_" + datetime.now().strftime('%H:%M')
+
+#check to see fi directory arleady exists
+if os.path.exists(path):
+    pass
+else:
+    os.mkdir(path)
+    #IF it doesn't exist, make it.
+
+interval = int(input("Time between pictures (in seconds): "))
+time_span = int(input("For how long (in minutes): "))
+counter = int((time_span*60)/interval)
+
+#camera.capture(path + '/1.jpg')
+#Now, how to increment this?
+
+for i in range(counter):
+    picture_name = path + '/%s.jpg' %i
+    camera.capture(picture_name)
+    time.sleep(interval)
+    
+
---
+++ TakePhoto.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import os
+from picamera import PiCamera
+
+#Take a photo
+#name it
+#add it to/var/www/html/
+
+camera = PiCamera()
+camera.resolution = (2592,1944)
+path = "/var/www/html/" + datetime.now().strftime('%Y%m%d')
+
+#check to see fi directory arleady exists
+if os.path.exists(path):
+    pass
+else:
+    os.mkdir(path)
+    #IF it doesn't exist, make it.
+
+#camera.capture(path + '/1.jpg')
+#Now, how to increment this?
+
+pic_num = 1
+picture_name = path + '/%s.jpg' %pic_num
+
+while os.path.exists(picture_name):#if the file already exists....
+    pic_num += 1
+    picture_name = path + '/%s.jpg' %pic_num
+    if os.path.exists(picture_name) == False:
+        break
+
+camera.capture(picture_name)
+    

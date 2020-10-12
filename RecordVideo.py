ed606737bf51ecdeb066dfdbbb395e50f50523cd
markgoodrich <mark.d.goodrich@gmail.com>
2020-10-12 08:27:39 -0500

    Added USB recording capability for Video

--- RecordVideo.py
+++ RecordVideo.py
@@ -6,8 +6,13 @@ import subprocess
 
 camera = PiCamera()
 camera.resolution = (1920, 1080)
-path = "/home/pi/Videos/" + datetime.now().strftime('%Y%m%d_%H:%M')
 
+usb_answer = input("Is the external USB inserted? [Y/n]: ")
+if usb_answer.upper() == "Y":
+	path = "/media/pi/PiCamera/" + datetime.now().strftime('%Y%m%d_%H:%M')
+	
+else:
+	path = "/home/pi/Videos/" + datetime.now().strftime('%Y%m%d_%H:%M')
 
 duration = int(input("How long to film (in seconds): "))
 
@@ -19,7 +24,7 @@ camera.start_recording(video_name)
 sleep(duration)
 camera.stop_recording()
 
-
+print("Video recording finished.")
 
 #subprocess.run(["scp", video_name, "markgoodrich@192.168.0.100:/home/markgoodrich/Videos/Pi_Videos/"])
 

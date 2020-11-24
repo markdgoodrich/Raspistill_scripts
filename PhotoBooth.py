dd882c92ea97c007167a7ba640ea255fbeb71fa5
markgoodrich <mark.d.goodrich@gmail.com>
2020-11-24 17:09:51 -0600

    Adding first Photobooth function

---
+++ PhotoBooth.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import os
+from picamera import PiCamera
+
+#Take a photo
+#name it
+#add it to/var/www/html/
+
+camera = PiCamera()
+camera.resolution = (4056,3040)
+path = "/var/www/html/" + datetime.now().strftime('%Y%m%d') + "_PhotoBooth"
+
+#check to see if directory arleady exists
+if os.path.exists(path):
+    pass
+else:
+    os.mkdir(path)
+
+pic_num = 1
+picture_name = path + '/%s.jpg' %pic_num
+
+
+
+#PhotoBooth
+#Have display up
+#Press 'enter' to take picture
+#   Path should be 
+
+button = input("Hit 'Enter' to take a photo!")
+camera.start_preview()
+
+while true:
+    if button == '':
+        camera.capture(picture_name)
+        pic_num += 1
